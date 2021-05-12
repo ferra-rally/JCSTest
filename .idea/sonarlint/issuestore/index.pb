64636adb616f0909d586b1dc55ed79e2c2f1373d
ferra-rally <danieleferrarelli@gmail.com>
2021-05-12 09:02:20 +0200

    Added parametrization of cache

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,13 +1,5 @@
 
-9
-	cache.ccf,c/d/cd435743387c7a8a0418c9a8eacac6a71e2c3880
 7
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
 B
 src/conf/cache.ccf,e/7/e73e08e3d65d2291338f45e9ef31888c0242a9c8
-s
-Csrc/test/java/it/uniroma2/dicii/isw2/jcs/paramTests/JCSUniTest.java,f/4/f4d44496ace37794812104a54e996e3c1385c47c
-x
-Hsrc/test/java/it/uniroma2/dicii/isw2/jcs/paramTests/RemovalTestUtil.java,8/2/82faf014dc6c0626ea021d6cd878541c98c957d4
-v
-Fsrc/test/java/it/uniroma2/dicii/isw2/jcs/paramTests/IntegrationIT.java,f/3/f3299752d020ea07361269ccad9ce2b004547877
--- src/test/java/it/uniroma2/dicii/isw2/jcs/paramTests/JCSUniTest.java
+++ src/test/java/it/uniroma2/dicii/isw2/jcs/paramTests/JCSUniTest.java
@@ -17,26 +17,29 @@ import java.util.*;
 @RunWith(value = Parameterized.class)
 public class JCSUniTest {
     private final Random random = new Random();
-    private int number = 100;
-    private String name;
+    private int number;
+    private String key;
+    private String cache;
     private JCS jcs;
 
-    public JCSUniTest(String name) {
-        this.name = name;
+    public JCSUniTest(String key, int number, String cache) {
+        this.key = key;
+        this.number = number;
+        this.cache = cache;
     }
 
     @Parameters
-    public static Collection<String[]> getTestParameters() {
-        return Arrays.asList(new String[][]{
-                {"some:key"},
-                {""},
-                {"Another:key"}
+    public static Collection<Object[]> getTestParameters() {
+        return Arrays.asList(new Object[][]{
+                {"some:key", 100, "testCache1"},
+                {"", 150, "testCache1"},
+                {"Another:key", 0, "testCache1"}
         });
     }
 
     @Before
     public void configure() throws CacheException {
-        jcs = JCS.getInstance("testCache1");
+        jcs = JCS.getInstance(cache);
     }
 
     @Test
@@ -45,9 +48,9 @@ public class JCSUniTest {
 
         LinkedList list = buildList(number);
 
-        jcs.put(name, list);
+        jcs.put(key, list);
 
-        Assert.assertEquals(list, jcs.get(name));
+        Assert.assertEquals(list, jcs.get(key));
     }
 
     private LinkedList buildList(int number) {
